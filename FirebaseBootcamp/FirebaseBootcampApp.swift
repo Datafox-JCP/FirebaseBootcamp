2a4b3b3c134e58c726584aeb219b4341380fab46
Datafox-JCP <datafox@outlook.com>
2023-04-02 08:07:01 -0600

    - Firebase Setup

--- FirebaseBootcamp/FirebaseBootcampApp.swift
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -6,12 +6,31 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct FirebaseBootcampApp: App {
+    
+    /// Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    ///  Firebase setup without delegate
+//    init() {
+//        FirebaseApp.configure()
+//        print("Cinfigured Forebase!")
+//    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
     }
 }
