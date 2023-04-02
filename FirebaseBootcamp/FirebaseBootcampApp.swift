dd3e79291429d3167da19a32c65bfee640ad809b
Datafox-JCP <datafox@outlook.com>
2023-04-02 07:27:31 -0600

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Juan Hernandez Pazos on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
